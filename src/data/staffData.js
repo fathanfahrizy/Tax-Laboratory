8be8286aa9cb658aacca2cdf44b79e46a3d10dd0
fathanfahrizy <fathanfahrizy@gmail.com>
2026-03-24 10:48:27 +0700

    Menghapus Expertise dari Staff

--- src/data/staffData.js
+++ src/data/staffData.js
@@ -3,21 +3,18 @@ export const STAFF_DATA = [
   {
     name: "Dr. Silvia Avira, SE., MM.",
     role: "Kepala Staff Laboratorium Akuntansi Lanjut B",
-    expertise: "Perpajakan Internasional & Akuntansi Publik",
     email: "budi.santoso@gunadarma.ac.id",
     img: "/img-staff/dosen1.png"
   },
   {
     name: "Irmawati Wijaya, S.E., MMSI.",
     role: "Staff Laboratorium Akuntansi Lanjut B",
-    expertise: "Hukum Pajak & Manajemen Risiko Terapan",
     email: "siti.aminah@gunadarma.ac.id",
     img: "/img-staff/dosen2.png"
   },
   {
     name: "Nicky Handayani, SE., MMSI.",
     role: "Staff Laboratorium Akuntansi Lanjut B",
-    expertise: "Hukum Pajak & Manajemen Risiko Terapan",
     email: "siti.aminah@gunadarma.ac.id",
     img: "/img-staff/dosen2.png"
   }
