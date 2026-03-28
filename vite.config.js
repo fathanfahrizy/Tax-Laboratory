d6ba214d50bac91bf6917034117206ba71293316
fathanfahrizy <fathanfahrizy@gmail.com>
2026-03-28 07:14:22 +0700

    Masukin sitemap, robots

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { VitePWA } from 'vite-plugin-pwa' // <-- Tambahin import ini
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(), // Tailwind v4 tetep aman di sini
+    tailwindcss(),
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg', 'logo-taxlab.webp'],
+      workbox: {
+        navigateFallbackDenylist: [/^\/sitemap\.xml$/, /^\/robots\.txt$/]
+      },
       manifest: {
         name: 'TaxLaboratory Gunadarma',
         short_name: 'TaxLab',
